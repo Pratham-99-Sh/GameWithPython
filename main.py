71bd76ce995799d80e5d50c47c7dbfe05d4c84aa
Pratham-99-Sh <ppratham1_be21@thapar.edu>
2023-07-26 23:12:52 +0000

    to display a window with a background

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import pygame
 import time
 import random
+
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My First Game")
+
+BG = pygame.image.load('bg.jpeg')
+
+def draw():
+    # blit is used to draw an image or surface to screen
+    WIN.blit(BG, (0, 0)) # (image, (coordinate to place top left corner of image))
+    pygame.display.update()
+
+def main():
+    run = True
+
+    while run:
+        # pygame.event.get() returns a list of all the events that have happened since the last time it was called
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw()
+
+    pygame.quit()
+
+
+# since we want to run the game only when this file is run and now when say it is imported``
+if __name__ == "__main__":
+    main()
