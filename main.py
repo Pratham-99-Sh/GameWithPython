11f81837f8752a8d06a13f2e15a5d2d33a321d02
Debian Live user <user@localhost.localdomain>
2023-07-27 23:29:22 +0000

    Just Left and Right Movement

--- main.py
+++ main.py
@@ -8,22 +8,41 @@ pygame.display.set_caption("My First Game")
 
 BG = pygame.transform.scale(pygame.image.load('bg.jpeg'), (WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH, PLAYER_HEIGHT = 40, 60 
+PLAYER_VEL = 5
+
+def draw(player):
     # blit is used to draw an image or surface to screen
     WIN.blit(BG, (0, 0)) # (image, (coordinate to place top left corner of image))
+
+    # (surface, color, rect) where color = string or (r, g, b)
+    pygame.draw.rect(WIN, (255, 0, 0), player)
+
     pygame.display.update()
 
 def main():
     run = True
 
+    # (x, y, width, height)
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60) # 60 fps for this while loop
         # pygame.event.get() returns a list of all the events that have happened since the last time it was called
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        draw()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x > 0:
+            player.x -= PLAYER_VEL
+
+        if keys[pygame.K_RIGHT] and player.x < WIDTH - PLAYER_WIDTH:
+            player.x += PLAYER_VEL
+
+        draw(player)
 
     pygame.quit()
 
