db8e7ab5ff46663fc5f2c05f8049948364492201
Debian Live user <user@localhost.localdomain>
2023-07-27 23:45:10 +0000

    Added Timer in game

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import time
 import random
 
+pygame.font.init() # initialize font module
+
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My First Game")
@@ -11,9 +13,14 @@ BG = pygame.transform.scale(pygame.image.load('bg.jpeg'), (WIDTH, HEIGHT))
 PLAYER_WIDTH, PLAYER_HEIGHT = 40, 60 
 PLAYER_VEL = 5
 
-def draw(player):
+FONT = pygame.font.SysFont('comicsans', 30)
+
+def draw(player, ellapsed_time):
     # blit is used to draw an image or surface to screen
     WIN.blit(BG, (0, 0)) # (image, (coordinate to place top left corner of image))
+
+    time_text = FONT.render(f"Time: {round(ellapsed_time)}s", 1, (255, 255, 255)) # (text (Round off the time), antialiasing, color) Color - str or rgb
+    WIN.blit(time_text, (10, 10)) # (text, (coordinate to place top left corner of text))
 
     # (surface, color, rect) where color = string or (r, g, b)
     pygame.draw.rect(WIN, (255, 0, 0), player)
@@ -26,9 +33,13 @@ def main():
     # (x, y, width, height)
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
     clock = pygame.time.Clock()
+    start_time = time.time()
+    ellapsed_time = 0
 
     while run:
         clock.tick(60) # 60 fps for this while loop
+        ellapsed_time = time.time() - start_time # time since start of game
+
         # pygame.event.get() returns a list of all the events that have happened since the last time it was called
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,7 +53,7 @@ def main():
         if keys[pygame.K_RIGHT] and player.x < WIDTH - PLAYER_WIDTH:
             player.x += PLAYER_VEL
 
-        draw(player)
+        draw(player, ellapsed_time)
 
     pygame.quit()
 
