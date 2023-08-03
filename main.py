0d10ddca853c5998c27961e35676767e5d38ebae
Debian Live user <user@localhost.localdomain>
2023-08-03 23:58:09 +0000

    Added Pause Functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 pygame.font.init() # initialize font module
 pygame.mixer.init() # initialize mixer module
 
+# pygame.mixer.music.load("music.mp3") # load music file
 crash_sound = pygame.mixer.Sound("crash.wav")
 
 WIDTH, HEIGHT = 1000, 800
@@ -36,14 +37,26 @@ def draw(player, ellapsed_time, stars):
 
     pygame.display.update()
 
+def draw_pause_screen():
+    # to make pause screen
+    s = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)   # per-pixel alpha
+    s.fill((255,255,255,128))                         # notice the alpha value in the color
+    WIN.blit(s, (0,0))                                # (0,0) are the top-left coordinates
+    pause_text = FONT.render("Paused", 1, (0, 0, 255))
+    WIN.blit(pause_text, (WIDTH / 2 - pause_text.get_width() / 2, HEIGHT / 2 - pause_text.get_height() / 2))
+    pygame.display.update()
+
 def main():
     run = True
+    pause = False  # variable for pausing and resuming the game
 
     # (x, y, width, height)
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
     clock = pygame.time.Clock()
     start_time = time.time()
     ellapsed_time = 0
+    time_adj = 0 # variable to adjust the time after pausing and resuming the game
+    ellipsed_time_old = 0 # variable to store the time before pausing the game
 
     star_add_increment = 2000 # in milliseconds
     star_count = 0 # variable to tell us when to add next star
@@ -53,16 +66,17 @@ def main():
 
     while run:
         star_count += clock.tick(60) # 60 fps for this while loop and it returns the time since the last tick
-        ellapsed_time = time.time() - start_time # time since start of game
+        ellapsed_time = time.time() - start_time - time_adj # time since start of game
 
         if star_count >= star_add_increment:
-            for _ in range(3):
-                star_x = random.randint(0, WIDTH - STAR_WIDTH)
-                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT) # (x, y, width, height) where y is - ve so that it starts above the screen
-                stars.append(star)
+            if pause == False:
+                for _ in range(3):
+                    star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                    star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT) # (x, y, width, height) where y is - ve so that it starts above the screen
+                    stars.append(star)
 
-            star_count = 0
-            star_add_increment = max(200, star_add_increment - 50) # decrease the time to add next star by 100ms
+                star_count = 0
+                star_add_increment = max(200, star_add_increment - 50) # decrease the time to add next star by 100ms
 
         # pygame.event.get() returns a list of all the events that have happened since the last time it was called
         for event in pygame.event.get():
@@ -77,14 +91,26 @@ def main():
         if keys[pygame.K_RIGHT] and player.x < WIDTH - PLAYER_WIDTH:
             player.x += PLAYER_VEL
 
-        for star in stars[:]:
-            star.y += STAR_VEL 
-            if star.y > HEIGHT:
-                stars.remove(star)
-            elif star.y + star.height >= player.y and star.colliderect(player): # only chekck for colliosn if the star is below the player
-                stars.remove(star)
-                hit = True
-                break
+        if keys[pygame.K_SPACE]:
+            if pause:
+                time_adj += ellapsed_time - ellipsed_time_old
+                pause = False
+            else:
+                ellipsed_time_old = ellapsed_time
+                draw_pause_screen()
+                pause = True
+
+            time.sleep(0.2) # to avoid multiple key presses
+
+        if pause == False:
+            for star in stars[:]:
+                star.y += STAR_VEL 
+                if star.y > HEIGHT:
+                    stars.remove(star)
+                elif star.y + star.height >= player.y and star.colliderect(player): # only chekck for colliosn if the star is below the player
+                    stars.remove(star)
+                    hit = True
+                    break
 
         if hit:
             crash_sound.play()
@@ -94,7 +120,8 @@ def main():
             pygame.time.delay(4000)
             break
 
-        draw(player, ellapsed_time, stars)
+        if pause == False:
+            draw(player, ellapsed_time, stars)
 
     pygame.quit()
 
